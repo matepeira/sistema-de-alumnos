251e6c8304186a769d8b27c54e589cf0a7b6c685
matepeira <mateopeirano21@gmail.com>
2026-08-31 15:56:53 -0300

    Etapa 1: La primera clase

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SistemaDeAlumnos;
+
+Alumno alumnoUno = new Alumno()
+{
+    Nombre = "Pepe",
+    Legajo = 1,
+    Nota1 = 7.0m,
+    Nota2 = 8.5m,
+};
+
+Alumno alumnoDos = new Alumno()
+{
+    Nombre = "Maria",
+    Legajo = 2,
+    Nota1 = 5.2m,
+    Nota2 = 9.5m,
+};
+
+Console.WriteLine($"Nombre del alumno: {alumnoUno.Nombre}, Legajo: {alumnoUno.Legajo}");
+Console.WriteLine($"Nombre del alumno: {alumnoDos.Nombre}, Legajo: {alumnoDos.Legajo}");
