2a699d1bbfb78a72dd4b90865c646434e692a06b
matepeira <mateopeirano21@gmail.com>
2026-08-31 15:59:14 -0300

    Etapa 2: Constructor

--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
 ﻿using SistemaDeAlumnos;
 
-Alumno alumnoUno = new Alumno()
-{
-    Nombre = "Pepe",
-    Legajo = 1,
-    Nota1 = 7.0m,
-    Nota2 = 8.5m,
-};
-
-Alumno alumnoDos = new Alumno()
-{
-    Nombre = "Maria",
-    Legajo = 2,
-    Nota1 = 5.2m,
-    Nota2 = 9.5m,
-};
+Alumno alumnoUno = new Alumno("Martin Ventu", 2, 9.2m, 4.0m);
+Alumno alumnoDos = new Alumno("Jose Sanchez", 1, 8.0m, 9.0m);
 
 Console.WriteLine($"Nombre del alumno: {alumnoUno.Nombre}, Legajo: {alumnoUno.Legajo}");
 Console.WriteLine($"Nombre del alumno: {alumnoDos.Nombre}, Legajo: {alumnoDos.Legajo}");
